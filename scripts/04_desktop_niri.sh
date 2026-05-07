c207decef7a939e8adaac8532b0896cb811aa4c0
Maomaokuxs <2788057316@qq.com>
2026-05-07 20:52:45 +0800

    1.使用gnome的图片查看器及文件管理器替代kde中的配套软件 2.补足cava软件包 3.补充软件仓库

--- scripts/04_desktop_niri.sh
+++ scripts/04_desktop_niri.sh
@@ -66,10 +66,9 @@ install_desktop_niri() {
     sudo dnf copr enable -y ackerman/nexus
 
     # 3. 执行补全安装
-    # 包含了之前可能被中断的 mako 和 dolphin
     # 核心：niri (窗口管理器), waybar (状态栏), rofi-wayland (启动器)
-    # 美化：swww (壁纸后端), hellwal (色彩方案生成), starship (终端美化)
-    # 功能：fcitx5 (输入法), dolphin (文件管理), mako (通知通知), hyprlock/idle (锁屏与休眠)
+    # 美化：swww (壁纸后端), hellwal (色彩方案生成), starship (终端美化)，cava（终端可视化音乐实现）
+    # 功能：fcitx5 (输入法), nautilus (文件管理), mako (通知通知), hyprlock/idle (锁屏与休眠)，loupe（图片查看器）
     local niri_pkgs=(
         niri waybar rofi-wayland stow unzip kitty 
         fastfetch jq ImageMagick swww hellwal 
@@ -77,7 +76,7 @@ install_desktop_niri() {
         gpu-screen-recorder libnotify mako 
         dolphin fcitx5 fcitx5-chinese-addons 
         xdg-desktop-portal-gnome xdg-desktop-portal-wlr
-        polkit-kde firfox 
+        polkit-kde firfox cava nautilus loupe
     )
 
     echo -e "${YELLOW}>> Deploying Niri ecosystem components...${NC}"
