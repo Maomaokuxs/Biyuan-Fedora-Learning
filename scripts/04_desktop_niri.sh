9101301112ccc637c2fc3030ab3e3bf581c6a947
Maomaokuxs <2788057316@qq.com>
2026-05-12 16:46:51 +0800

    添加一些实用的软件包

--- scripts/04_desktop_niri.sh
+++ scripts/04_desktop_niri.sh
@@ -70,6 +70,7 @@ install_desktop_niri() {
     # 美化：swww (壁纸后端), hellwal (色彩方案生成), starship (终端美化)，cava（终端可视化音乐实现）
     # 功能：fcitx5 (输入法), nautilus (文件管理), mako (通知通知), hyprlock/idle (锁屏与休眠)，loupe（图片查看器），
     # blueman（蓝牙管理工具），btop（资源占用查看工具)，gnome-text-editor（文本编辑器）,ddcutil(显示器亮度调整)，nmtui（网络连接工具）
+    # seahorse gnome-keyring libsecret（密钥及管理工具）
     
     local niri_pkgs=(
         niri waybar rofi-wayland stow unzip kitty 
@@ -79,7 +80,8 @@ install_desktop_niri() {
         dolphin fcitx5 fcitx5-chinese-addons 
         xdg-desktop-portal-gnome xdg-desktop-portal-wlr
         polkit-kde firfox cava nautilus loupe nautilus-python
-        blueman btop gnome-text-editor ddcutil nmtui
+        blueman btop gnome-text-editor ddcutil nmtui 
+        seahorse gnome-keyring libsecret
     )
 
     echo -e "${YELLOW}>> Deploying Niri ecosystem components...${NC}"
