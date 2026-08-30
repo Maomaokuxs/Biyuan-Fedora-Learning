449bb66d4f24b072a4e96c9ae963e465617f52c1
Maomaokuxs <biyuanh@qq.com>
2026-08-30 23:45:27 +0800

    修改：优化亮度数值显示

--- dotfiles/waybar/.config/waybar/scripts/brightness.sh
+++ dotfiles/waybar/.config/waybar/scripts/brightness.sh
@@ -7,6 +7,6 @@ set_bl() { busctl call org.freedesktop.login1 /org/freedesktop/login1/session/au
 case "$1" in
     up)   set_bl $(( B + 1310 > M ? M : B + 1310 ));;
     down) set_bl $(( B - 1310 < 0 ? 0 : B - 1310 ));;
-    mid)  set_bl $(( M / 2 ));;
-    *)    p=$(( B * 100 / M )); echo "{\"percentage\": $p}";;
+    mid)  set_bl $(( (M + 1) / 2 ));;
+    *)    p=$(( (B * 100 + M/2) / M )); echo "{\"percentage\": $p}";;
 esac
